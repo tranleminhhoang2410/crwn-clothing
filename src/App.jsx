ec319e30cf7a1632e49407f0300288cb16e0145d
Le Minh Hoang Tran <hoangtlm2410@gmail.com>
2023-07-21 16:39:20 +0700

    implement category item component

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,12 @@
 import './categories.styles.scss';
+import CategoryItem from './components/category-item/category-item.component';
+import categories from '../categories.json';
 
 const App = () => {
-    const categories = [
-        {
-            id: 1,
-            title: 'Hats',
-        },
-        {
-            id: 2,
-            title: 'Jackets',
-        },
-        {
-            id: 3,
-            title: 'Sneakers',
-        },
-        {
-            id: 4,
-            title: 'Womens',
-        },
-        {
-            id: 5,
-            title: 'Mens',
-        },
-    ];
-
     return (
         <div className="categories-container">
-            {categories.map(({ id, title }) => (
-                <div key={id} className="category-container">
-                    <div className="background-image" />
-                    <div className="category-body-container">
-                        <h2>{title}</h2>
-                        <p>Shop</p>
-                    </div>
-                </div>
+            {categories.map((category) => (
+                <CategoryItem key={category.id} category={category} />
             ))}
         </div>
     );
