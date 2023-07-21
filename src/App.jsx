9454fe26d835da8f5e79f53e82012eea2336aaa9
Le Minh Hoang Tran <82258999+tranleminhhoang2410@users.noreply.github.com>
2023-07-21 17:02:49 +0700

    implement directory component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import './categories.styles.scss';
-import CategoryItem from './components/category-item/category-item.component';
 import categories from '../categories.json';
+import Directory from './components/directory/directory.component';
 
 const App = () => {
-    return (
-        <div className="categories-container">
-            {categories.map((category) => (
-                <CategoryItem key={category.id} category={category} />
-            ))}
-        </div>
-    );
+    return <Directory categories={categories} />;
 };
 
 export default App;
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+import CategoryItem from '../category-item/category-item.component';
+
+import './directory.styles.scss';
+
+const Directory = ({ categories }) => {
+    return (
+        <div className="directory-container">
+            {categories.map((category) => (
+                <CategoryItem key={category.id} category={category} />
+            ))}
+        </div>
+    );
+};
+
+export default Directory;
