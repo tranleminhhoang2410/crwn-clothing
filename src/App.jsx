385dc0abc0b19d378844be66e3dfc83fe63a6b56
sam <84348013+sammie189@users.noreply.github.com>
2023-07-21 16:16:57 +0700

    implement base categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
+    const categories = [
+        {
+            id: 1,
+            title: 'Hats',
+        },
+        {
+            id: 2,
+            title: 'Jackets',
+        },
+        {
+            id: 3,
+            title: 'Sneakers',
+        },
+        {
+            id: 4,
+            title: 'Womens',
+        },
+        {
+            id: 5,
+            title: 'Mens',
+        },
+    ];
 
-function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <div className="categories-container">
+            {categories.map(({ title }) => (
+                <div className="category-container">
+                    <div className="background-image" />
+                    <div className="category-body-container">
+                        <h2>{title}</h2>
+                        <p>Shop</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
