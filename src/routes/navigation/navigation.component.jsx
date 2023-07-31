6f6e1e55fdd6ef9c02a0ba7da8ed5ed4876fa521
Le Minh Hoang Tran <82258999+tranleminhhoang2410@users.noreply.github.com>
2023-07-31 23:47:34 +0700

    implement cart icon and cart dropdowwn component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,6 +6,9 @@ import { UserContext } from '../../contexts/user.context';
 
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
 import './navigation.styles.scss';
 
 const Navigation = () => {
@@ -42,7 +45,9 @@ const Navigation = () => {
                             SIGN IN
                         </Link>
                     )}
+                    <CartIcon />
                 </div>
+                <CartDropdown />
             </div>
             <Outlet />
         </>
